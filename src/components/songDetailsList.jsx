3a38fb7f4df9e55e5a5ae7901513690149400ad9
karthicbz <pkarthicbz@live.in>
2023-05-11 21:29:57 +0530

    change flex to grid

--- src/components/songDetailsList.jsx
+++ src/components/songDetailsList.jsx
@@ -8,7 +8,6 @@ const SongDetailsList = ({songDetails})=>{
                     <div className="song-info">
                         <p>Name: {details.album.name}</p>
                         <p>Duration: {(parseInt(details.duration)/60).toFixed(2)}</p>
-                        <p>Artist: {details.featuredArtists}</p>
                         {/* <button type="submit" onClick={window.open(details.downloadUrl[4].link)}>Download</button> */}
                         <a href={details.downloadUrl[4].link} download='song' target="_blank" rel="noopener noreferrer">Download</a>
                         {/* <form method="get" action={details.downloadUrl[4].link}>
